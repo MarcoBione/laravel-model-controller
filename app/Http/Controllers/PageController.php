e8ba4a8027da95ae19d447769e861745b4aa22eb
MarcoBione <marcobione.work@gmail.com>
2023-05-23 16:36:02 +0200

    Issue PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
         $title='HomePage';
+        $movies = Movie::all();
+        dd($movies);
         return view('welcome', compact('title'));
     }
+
+
 }
